581077e8eaf99c3220747c100af50bb8bb08a3af
Julien Terrier <legzomil@gmail.com>
2011-10-19 23:33:31 +0200

    Added logback dependency & conf

--- src/main/java/org/elitefactory/paramz/model/Parameter.java
+++ src/main/java/org/elitefactory/paramz/model/Parameter.java
@@ -1,7 +1,34 @@
 package org.elitefactory.paramz.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Parameter {
 
 	private String name;
 
+	private String value;
+
+	private final static Logger logger = LoggerFactory
+			.getLogger(Parameter.class);
+
+	public Parameter() {
+		logger.debug("Instantiated parameter");
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
 }
--- src/test/java/org/elitefactory/paramz/ParameterTest.java
+++ src/test/java/org/elitefactory/paramz/ParameterTest.java
@@ -1,12 +1,13 @@
 package org.elitefactory.paramz;
 
+import org.elitefactory.paramz.model.Parameter;
 import org.junit.Test;
 
 public class ParameterTest {
 
 	@Test
 	public void dummy() {
-
+		new Parameter();
 	}
 
 }
